55fe19cedcd8fd580bd97570b2bb0d4e66f306c1
PatrickStar091514 <93037943+PatrickStar091514@users.noreply.github.com>
2022-01-22 23:05:09 +0800

    Update: Boss Controler
    更新了敌人脚本，和敌人子弹脚本，重置了敌人子弹的预制体，添加了测试敌人预制体

--- GGJ-01/GGJ-01/Assets/Script/Bullet/EnemyBullet.cs
+++ GGJ-01/GGJ-01/Assets/Script/Bullet/EnemyBullet.cs
@@ -7,10 +7,19 @@ public class EnemyBullet : MonoBehaviour
     public GameObject uiManager;
     public GameObject enemyHPManager;
 
+    public float bulletSpeed;
+    public Rigidbody2D bulletRb;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        bulletSpeed = Random.Range(1.0f, 2.0f);
+        GameObject dadObject = this.transform.parent.gameObject;
+        if (dadObject.name == "LeftGun") bulletSpeed = -bulletSpeed;
+        //Debug.Log(dadObject.name);
+        //Debug.Log(bulletSpeed);
+        bulletRb = GetComponent<Rigidbody2D>();
+        bulletRb.velocity = new Vector3(bulletSpeed, 0, 0);
     }
 
     // Update is called once per frame
